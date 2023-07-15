5933814b3406f56b542214422e364f31b3cad310
Nur Fatiha Sartika Jimmy <tika@Nurs-MacBook-Air.local>
2023-07-15 15:23:53 +0700

    update url helper

--- client/src/helper.js
+++ client/src/helper.js
@@ -1,4 +1,3 @@
-export const API_URL = "http://localhost:3232";
-// export const API_URL = "https://podscript.herokuapp.com";
-// export const API_URL = "https://master--marvelous-brigadeiros-1fcac4.netlify.app/";
+// export const API_URL = "http://localhost:3232";
+export const API_URL = "https://podscript-aa66b7368e1f.herokuapp.com/";
 
